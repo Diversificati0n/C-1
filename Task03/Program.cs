d93e4f8a077b24357f989bf38825871564563660
Diversification <startsevprm@gmail.com>
2022-12-19 20:50:44 +0500

    add

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,14 +1,39 @@
-﻿// 5. Напишите программу, которая на вход принимает
-// одно число (N), а на выходе показывает все целые
-// числа в промежутке от -N до N.
-// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
-// 2 -> " -2, -1, 0, 1, 2"
+﻿// 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
 
-Console.WriteLine("Введите число");
-int number = Convert.ToInt32(Console.ReadLine());
-int count = -number;
-while (count <= number)
+Console.WriteLine("Введите число от 1 до 7"); // выводит в терминал текст
+string a = Console.ReadLine(); // записывает в number число введенное пользователем в терминал и переводит в init32
+if (a == "1")
 {
-    Console.Write(count + " ");
-    count++;
+    Console.WriteLine("понедельник");
+}
+else if (a == "2")
+{
+    Console.WriteLine("вторник");
+}
+else if (a == "3")
+{
+    Console.WriteLine("среда");
+}
+else if (a == "4")
+{
+    Console.WriteLine("четверг");
+}
+else if (a == "5")
+{
+    Console.WriteLine("пятница");
+}
+else if (a == "6")
+{
+    Console.WriteLine("суббота");
+}
+else if (a == "7")
+{
+    Console.WriteLine("воскресенье");
+}
+else
+{
+    Console.WriteLine("Введены неправильные данные");
 }
