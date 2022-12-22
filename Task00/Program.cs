e67dee236193ddc707499c17febe7e1342f35768
Diversification <startsevprm@gmail.com>
2022-12-22 21:48:09 +0500

    исправиления в Task00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -6,8 +6,8 @@
 // 4 -> 16
 // -3 -> 9
 // -7 -> 49
-Console.Write("Введите целое число");
+Console.WriteLine("Введите целое число");
 //string str = Console.ReadLine(); 134864966
 int number = Convert.ToInt32(Console.ReadLine());
 int square = number * number;
-Console.WriteLine($"Квадрат числа {number} = {square}");
+Console.WriteLine($"Квадрат числа {number} = {square}"); // $ - интерполяция {переменные} 
