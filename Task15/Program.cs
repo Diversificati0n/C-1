f37964af96c331775cd9bb632a157d441b5db219
Diversification <startsevprm@gmail.com>
2022-12-26 18:48:51 +0500

    problem 15 solution

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -3,3 +3,16 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.Write("Введи цифру от 1 до 7 (день недели): ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+void CheckingDay (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("выходной день");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("такого дня недели нет");
+  }
+  else Console.WriteLine("это не выходной день");
+}
+CheckingDay(dayNumber);
