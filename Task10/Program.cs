9e487407d3e39b857693233a9cd701353231c8cf
Diversification <startsevprm@gmail.com>
2022-12-26 18:21:04 +0500

    problem 10 solution

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -4,3 +4,14 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+
+
+// Console.Write("Введите трёхзначное число: ");
+// int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+// string strNumber = Convert.ToString(threeDigitNumber);
+// Console.WriteLine("вторая цифра этого числа -> "+strNumber[1]); 
+
+Console.WriteLine("Введите трёхзначное число: ");
+string str = Convert.ToString(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine("Вторая цифра: {0}", (str.Length > 1) ? str.Substring(str.Length - 2, 1) : " ");
