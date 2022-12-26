9bc066acd3b254d320a377df79c4d3ea3b0282ce
Diversification <startsevprm@gmail.com>
2022-12-26 18:33:41 +0500

    problem 13 solution

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -3,3 +3,14 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+
+Console.Write("Введи число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+string number1Text = Convert.ToString(number1);
+if (number1Text.Length > 2){
+  Console.WriteLine("третья цифра" + number1Text[2]);
+}
+else {
+  Console.WriteLine("третьей цифры нет");
+}
