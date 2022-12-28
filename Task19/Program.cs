4f926e3bf37e5e0f8a68d5e284ad05533c9628f4
Diversification <startsevprm@gmail.com>
2022-12-28 18:03:42 +0500

    file fix Task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -5,3 +5,23 @@
 // 12821 -> да
 // 23432 -> да
 
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 10000 & number <= 99999)
+{
+    Console.WriteLine($"{number} ");
+    Console.WriteLine(Palindrome(number) ? "да" : "нет");
+}
+else
+{
+    Console.WriteLine("неверное число");
+}
+
+bool Palindrome(int number1)
+{
+    if (number / 10000 == number % 10 & number / 1000 % 10 == number / 10 % 10)
+        return true;
+    else return false;
+}
