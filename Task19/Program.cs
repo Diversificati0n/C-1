7994cbf6d8e3a0c2db270d61fec3f9b3317b7e56
Diversification <startsevprm@gmail.com>
2022-12-28 18:13:10 +0500

    file fix Task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -14,14 +14,11 @@ if (number >= 10000 & number <= 99999)
     Console.WriteLine($"{number} ");
     Console.WriteLine(Palindrome(number) ? "да" : "нет");
 }
-else
-{
-    Console.WriteLine("неверное число");
-}
 
 bool Palindrome(int number1)
 {
-    if (number / 10000 == number % 10 & number / 1000 % 10 == number / 10 % 10)
+    if (number / 10000 == number % 10 &&
+        number / 1000 % 10 == number / 10 % 10)
         return true;
     else return false;
 }
