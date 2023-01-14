d4d153b215a5a8ebfdc2c421d0d76df9f0f786a1
Diversification <startsevprm@gmail.com>
2023-01-15 00:14:05 +0500

    Add 24,26,28,30

---
+++ Task28/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+// Console.WriteLine("Введите целое число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"{number} -> {SumNumbers(number)}");
+
+// int SumNumbers(int num)
+// {
+//     int sum = 1;
+//     for (int i = 1; i <= num; i++)
+//     {
+//         sum *= i;
+//     }
+//     return sum;
+// }
+
+
+Console.Write ("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// if number <= 0
+
+int factorialNumber = FactorialNumber(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {factorialNumber}");
+
+int FactorialNumber(int num)
+{
+    int result = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+        result *= i;
+        }
+    }
+    return result;
+}
---
+++ Task30/Program.cs
@@ -0,0 +1,64 @@
+﻿// int[] array = new int[8]; // 0,1,2,3,4,5,6,7
+
+// array[0] = 235;
+// array[1] = 33;
+// array[2] = 56;
+// array[3] = -35;
+// System.Console.WriteLine(array[2]);
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = 2;
+// }
+
+// int[] array1 = new int[5] { 2, 5, 7, 9, 0 };
+// int[] array2 = new int[] { 2, 5, 7, 9, 0 };
+// int[] array3 = { 2, 5, 7, 9, 0 };
+// int[] array4 = new [] { 2, 5, 7, 9, 0 };
+
+// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+// int[] array = new int[8]; // 0,1,2,3,4,5,6,7
+// FillArray(array);
+// PrintArray(array);
+
+// void FillArray(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = new Random().Next(0, 2);
+//     }
+// }
+
+// void PrintArray(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         Console.Write(arr[i] + " ");
+//     }
+// }
+
+
+int[] array = new int[8]; // 0,1,2,3,4,5,6,7
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] arr)
+{
+    Random rnd = new Random(); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 2); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
