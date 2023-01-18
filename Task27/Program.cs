f7bb18fa5a10ac66f974022ed0fd727bb4584dda
Diversification <startsevprm@gmail.com>
2023-01-18 12:28:22 +0500

    add Task 25,27,29

---
+++ Task27/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumNumbers = SumNumbers(number);
+Console.WriteLine("Сумма цифр в числе: " + sumNumbers);
+
+if(number >= 0)
+{
+    Console.WriteLine();
+}
+else
+{
+    Console.WriteLine("Ошибка! Введите положительное число");
+}
+
+int SumNumbers(int number)
+{
+int counter = Convert.ToString(number).Length;
+int movement = 0;
+int result = 0;
+
+for (int i = 0; i < counter; i++)
+    {
+        movement = number - number % 10;
+        result = result + (number - movement);
+        number = number / 10;
+    }
+        return result;
+}
---
+++ Task29/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 
+//8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+int [] array = CreateArray(8);
+PrintArray(array);
+
+int [] CreateArray(int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 100);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+}
+
+
+// int[] array = new int[8];
+// Console.Write("[ ");
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(0, 100);
+//     Console.Write(array1(i) + ", ");
+// }
+// Console.Write("]");
+
+// int array1(int a)
+// {
+//     return array[a];
+// }
+
