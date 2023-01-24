15a1a71af5214fff95f5d2f758b1730284bc83e8
Diversification <startsevprm@gmail.com>
2023-01-24 16:49:22 +0500

    add Task 39, 40, 42, 44, 45

---
+++ Task42/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+string res = ConvertDecToBin(13);
+Console.WriteLine(res);
+
+string ConvertDecToBin(int num)
+{
+    string result = string.Empty;
+    while (num > 0)
+    {
+        result = num % 2 + result;
+        num = num / 2;
+    }
+    return result;
+}
